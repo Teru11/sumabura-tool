b4e206b0cffbb48c1a4e6bd1ca29fd6dd8f45e3b
teruteru <atsuatsu1130@gmail.com>
2025-04-06 22:11:57 +0900

    相手情報　レイアウト　完了

--- sumabura-web/src/components/form_fighter_info.vue
+++ sumabura-web/src/components/form_fighter_info.vue
@@ -1,12 +1,128 @@
 <!--【F】戦績フォーム -->
 <template>
-  <div class="from-fighter-info debug">
-    <p>戦績</p>
+  <div class="from-fighter-info">
+    <div class="fighter-info-select">
+      <div class="title">相手情報</div>
+      <div class="search">
+        <!-- 略称検索 -->
+        <form>
+          <input type="text" id="nickname" placeholder="略称検索" />
+          <button type="submit" name="search">検索</button>
+        </form>
+      </div>
+      <div class="message">*****のメモを更新しました。</div>
+    </div>
+
+    <div class="fighter-info">
+      <div class="fighter-table">
+        <table>
+          <tbody>
+            <tr>
+              <td class="fighter-image"><img src="@/assets/img/fighter/12.png"></td>
+              <td class="fighter-name">キャプテン・ファルコン</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="winloss-table">
+          <form>
+            <div class="data-row">
+              <span>勝数</span>
+              <span>487<button type="submit" class="win-btn" name="win">勝利</button></span>
+            </div>
+            <div class="data-row">
+              <span>負数</span>
+              <span>364<button type="submit" class="loss-btn" name="loss">敗北</button></span>
+            </div>
+            <div class="data-row">
+              <span>勝率</span><span>57%</span>
+            </div>
+            <div class="data-row">
+              <span>出現率</span><span>57%</span>
+            </div>
+          </form>		
+        </div> 
+      </div>
+
+      <div class="memo">
+        <form>
+          <div class="memo-header">
+            <div>ファイターメモ</div>
+            <button type="submit" name="update">更新</button>
+          </div>
+          <textarea></textarea>
+        </form>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
 .from-fighter-info {
   margin-bottom: 20px;
+}
+.fighter-info-select {
+  display: flex;
+  white-space: nowrap;
+}
+.fighter-info-select .title {
+  margin-top: 2px;
+  font-weight: 700;
+}
+.fighter-info {
+  display: flex;
+}
+.winloss-table {
+  margin-top: 10px;
+  margin-left: 30px;
+  width: 80%;
+  white-space: nowrap;
+}
+.winloss-table .data-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+}
+.winloss-table .data-row span {
+  flex: 1;
+  align-items: center;
+  color: black;
+  margin-top: 4px;
+  font-size: 14px;
+}
+.winloss-table .data-row button {
+  align-items: center;
+  vertical-align: baseline; /* middleをリセット */
+  margin-left: 18px;
+  height: 25px;
+}
+.winloss-table .data-row .win-btn {
+  color: black;
+  background-color: rgb(255, 136, 0);
+}
+.winloss-table .data-row .loss-btn {
+  color: #fff;
+  background-color: #007bff;
+}
+.message {
+  color: red;
+  margin-left: 20px;
+}
+.memo {
+  width: 100%;
+  margin-left: 20px;
+}
+.memo .memo-header {
+  display: flex;  
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 8px;
+}
+.memo textarea {
+  height: 200px;
+}
+/** input */
+#nickname {
+  width: 120px;
 }
 </style>
